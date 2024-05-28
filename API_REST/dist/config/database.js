fa795f2e4d55e51e8c32c9920b2730b5dbcc1c6a
MarcosHenrique <marcoshenriqueseixas@gmail.com>
2024-05-28 17:28:50 -0300

    fix database address

--- API_REST/dist/config/database.js
+++ API_REST/dist/config/database.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-  dialect: 'mysql',
+  dialect: 'mariadb',
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
