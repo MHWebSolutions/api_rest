a87c3dc7017aa004ec344054e9e9e086716a1869
MHWebSolutions <markinsgoleirao@gmail.com>
2024-06-13 21:53:44 -0300

    final fix,i hope

--- API_REST/dist/app.js
+++ API_REST/dist/app.js
@@ -7,11 +7,27 @@ var _express = require('express'); var _express2 = _interopRequireDefault(_expre
 var _path = require('path'); var _path2 = _interopRequireDefault(_path);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
+// import helmet from 'helmet';
 var _homeRouter = require('./routes/homeRouter'); var _homeRouter2 = _interopRequireDefault(_homeRouter);
 var _UserRouter = require('./routes/UserRouter'); var _UserRouter2 = _interopRequireDefault(_UserRouter);
 var _TokenRouter = require('./routes/TokenRouter'); var _TokenRouter2 = _interopRequireDefault(_TokenRouter);
 var _AlunoRouter = require('./routes/AlunoRouter'); var _AlunoRouter2 = _interopRequireDefault(_AlunoRouter);
 var _FotoRouter = require('./routes/FotoRouter'); var _FotoRouter2 = _interopRequireDefault(_FotoRouter);
+
+const whiteList = [
+  'https://master.dy5trvb93m8oj.amplifyapp.com',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -21,7 +37,7 @@ class App {
   }
 
   middlewares() {
-    this.app.use(_cors2.default.call(void 0, ));
+    this.app.use(_cors2.default.call(void 0, corsOptions));
     this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
--- API_REST/src/app.js
+++ API_REST/src/app.js
@@ -7,11 +7,27 @@ import express from 'express';
 import path, { resolve } from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
+// import helmet from 'helmet';
 import routes from './routes/homeRouter';
 import Userroutes from './routes/UserRouter';
 import TokenRouter from './routes/TokenRouter';
 import AlunoRouter from './routes/AlunoRouter';
 import FotoRouter from './routes/FotoRouter';
+
+const whiteList = [
+  'https://master.dy5trvb93m8oj.amplifyapp.com',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -21,7 +37,7 @@ class App {
   }
 
   middlewares() {
-    this.app.use(cors());
+    this.app.use(cors(corsOptions));
     this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
